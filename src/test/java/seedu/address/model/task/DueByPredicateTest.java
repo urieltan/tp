40893e30a8e29c0ace9de5a76f7636ac6c6aa39f
lerxcl <lerx98@gmail.com>
2020-10-14 16:06:42 +0800

    Add DueByPredicateTest

--- src/test/java/seedu/address/model/task/DueByPredicateTest.java
+++ src/test/java/seedu/address/model/task/DueByPredicateTest.java
@@ -8,21 +8,22 @@ import org.junit.jupiter.api.Test;
 import seedu.address.testutil.TodoBuilder;
 import seedu.address.testutil.EventBuilder;
 
-public class DueBeforePredicateTest {
+public class DueByPredicateTest {
     @Test
-    public void test_tasksDueBefore_returnsTrue() {
-        DueBeforePredicate predicate = new DueBeforePredicate("10-12-2020 2359");
+    public void test_tasksDueBy_returnsTrue() {
+        DueByPredicate predicate = new DueByPredicate("12-12-2020 2359");
 
         //To-do (homework)
         assertTrue(predicate.test(new TodoBuilder().build()));
 
         //Event (meeting)
+        predicate = new DueByPredicate("12-12-2020 1130");
         assertTrue(predicate.test(new EventBuilder().build()));
     }
 
     @Test
-    public void test_noTasksDueBefore_returnsFalse() {
-        DueBeforePredicate predicate = new DueBeforePredicate("15-12-2020 2359");
+    public void test_noTasksDueBy_returnsFalse() {
+        DueByPredicate predicate = new DueByPredicate("15-12-2020 2359");
 
         //To-do
         assertFalse(predicate.test(new TodoBuilder().build()));
