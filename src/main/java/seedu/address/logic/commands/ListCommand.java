6d78dc8ef3d2715482b274802693243700017c1c
Uriel Tan <urieltan3@gmail.com>
2020-10-29 22:58:48 +0800

    Adjust whitespace

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 public abstract class ListCommand extends Command {
-
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": List all items in events, todos or contacts"
