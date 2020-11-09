2920ae710dbe769219d4db5c93d081b318b0ff37
Uriel Tan <urieltan3@gmail.com>
2020-11-09 14:27:49 +0800

    Add constraint to description length

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -6,9 +6,20 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.tag.Tag;
 
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public abstract class Task {
+    /** Maximum length for description supported by Lifebook */
+    private static final int MAX_DESCRIPTION_LENGTH = 30;
+
+    /** Message for denoting max description length */
+    public static final String MESSAGE_CONSTRAINTS = "Description must have less than "
+            + MAX_DESCRIPTION_LENGTH + " characters.";
+
+
     /** A brief description of the task. */
     protected String description;
 
@@ -32,6 +43,7 @@ public abstract class Task {
     public Task(String description, Set<Tag> tags) {
         assert description != null;
         assert tags != null;
+        checkArgument(isValidDescription(description), MESSAGE_CONSTRAINTS);
         this.description = description;
         this.isDone = false;
         this.tags.addAll(tags);
@@ -47,6 +59,10 @@ public abstract class Task {
     public Task(boolean isDone, String description, Set<Tag> tags) {
         assert description != null;
         assert tags != null;
+        if(!isValidDescription(description)){
+            System.out.println(description);
+        }
+        checkArgument(isValidDescription(description), MESSAGE_CONSTRAINTS);
         this.description = description;
         this.isDone = isDone;
         this.tags.addAll(tags);
@@ -59,6 +75,7 @@ public abstract class Task {
      */
     public Task(String description) {
         assert description != null;
+        checkArgument(isValidDescription(description), MESSAGE_CONSTRAINTS);
         this.description = description;
         this.isDone = false;
     }
@@ -173,6 +190,14 @@ public abstract class Task {
     public boolean isRecurring() {
         System.out.println(this.getRecurrence() != null);
         return this.getRecurrence() != null;
+    }
+
+    /**
+     * Indicates if description is valid.
+     * @return true if it is valid, and false otherwise.
+     */
+    public static boolean isValidDescription(String description) {
+        return description.length() <= MAX_DESCRIPTION_LENGTH;
     }
 
     public abstract LocalDateTime getDeadline();
